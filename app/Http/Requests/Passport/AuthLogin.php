e96823d408bd1732c815909c1c17bf698d2eb7c6
parorisim <parorisim@gmail.com>
2026-06-13 01:47:40 +0800

    fix: remove login password length validation

--- app/Http/Requests/Passport/AuthLogin.php
+++ app/Http/Requests/Passport/AuthLogin.php
@@ -15,7 +15,7 @@ class AuthLogin extends FormRequest
     {
         return [
             'email' => 'required|email:strict',
-            'password' => 'required|min:8'
+            'password' => 'required'
         ];
     }
 
@@ -24,8 +24,7 @@ class AuthLogin extends FormRequest
         return [
             'email.required' => __('Email can not be empty'),
             'email.email' => __('Email format is incorrect'),
-            'password.required' => __('Password can not be empty'),
-            'password.min' => __('Password must be greater than 8 digits')
+            'password.required' => __('Password can not be empty')
         ];
     }
 }
